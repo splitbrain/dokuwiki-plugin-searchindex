886ceaef1d54b9ab648b62ae24cb0def3652497f
Andy Phillip <andy@madspunky.net>
2010-08-20 13:06:47 +0200

    German translation

---
+++ lang/de/lang.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * german language file
+ */
+
+// einstellungen muessen vorhenden und der sprache angemessen gesetzt sein
+$lang['encoding']   = 'utf-8';
+$lang['direction']  = 'ltr';
+
+// die menu-ausgabe fuer das admin-plugin
+// wenn dies hier gesetzt wird, muss das plugin die funktion getMenuText()
+// nicht ueberschreiben
+$lang['menu'] = 'Suchindex Manager';
+
+// custom language strings for the plugin
+
+//Setup VIM: ex: et ts=4 enc=utf-8 :
