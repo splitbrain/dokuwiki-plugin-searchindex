f0f9547680f5eb5fa4772665b6f94a83fcca3859
Andreas Gohr <andi@splitbrain.org>
2010-08-20 14:50:44 +0200

    Russian language update

--- lang/ru/lang.php
+++ lang/ru/lang.php
@@ -1,16 +1,18 @@
 <?php
 /**
- *russian language file
- * Alexander Sorkin aka Kibi <kibizoid [at] gmail [dot] com>
+ * russian language file
+ * @author Alexander Sorkin aka Kibi <kibizoid [at] gmail [dot] com>
  */
 
-// settings must be present and set appropriately for the language
-$lang['encoding']   = 'utf-8';
-$lang['direction']  = 'ltr';
 
-// for admin plugins, the menu prompt to be displayed in the admin menu
-// if set here, the plugin doesn't need to override the getMenuText() method
 $lang['menu'] = 'Менеджер индекса полнотекстового поиска';
 
-// custom language strings for the plugin
+$lang['js']['rebuild'] = 'Перестроить индекс';
+
+$lang['js']['indexing'] = 'Индексация:';
+$lang['js']['done']     = 'Индексирование окончено';
+$lang['js']['pages']    = '%d Найдено страниц';
+$lang['js']['clearing'] = 'Очистка индекса...';
+$lang['js']['finding']  = 'Составление списка всех страниц';
+
 //Setup VIM: ex: et ts=4 enc=utf-8 :
