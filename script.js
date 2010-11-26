270da59d8cee274c558a53dde02907c03277b4c1
Andreas Gohr <andi@splitbrain.org>
2010-11-26 16:28:08 +0100

    rename global var pl_si to plugin_searchindex. closes #5
    Other plugin authors (point in case: autolink3) seem to simply copying
    my AJAX code without much understanding, reusing my variable name. Which
    of course breaks when both plugins are installed.

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
  * @author Andreas Gohr <andi@splitbrain.org>
  */
 
-var pl_si = {
+var plugin_searchindex = {
 
     // hold some values
     pages: null,
@@ -19,24 +19,24 @@ var pl_si = {
      * initialize everything
      */
     init: function(){
-        pl_si.output = $('plugin__searchindex');
-        if(!pl_si.output) return;
+        plugin_searchindex.output = $('plugin__searchindex');
+        if(!plugin_searchindex.output) return;
 
-        pl_si.sack = new sack(DOKU_BASE + 'lib/plugins/searchindex/ajax.php');
-        pl_si.sack.AjaxFailedAlert = '';
-        pl_si.sack.encodeURIString = false;
-        pl_si.lang = LANG.plugins.searchindex;
+        plugin_searchindex.sack = new sack(DOKU_BASE + 'lib/plugins/searchindex/ajax.php');
+        plugin_searchindex.sack.AjaxFailedAlert = '';
+        plugin_searchindex.sack.encodeURIString = false;
+        plugin_searchindex.lang = LANG.plugins.searchindex;
 
         // init interface
-        pl_si.status('<button id="plugin__searchindex_btn" class="button">'+pl_si.lang.rebuild+'</button>');
-        addEvent($('plugin__searchindex_btn'),'click',pl_si.go);
+        plugin_searchindex.status('<button id="plugin__searchindex_btn" class="button">'+plugin_searchindex.lang.rebuild+'</button>');
+        addEvent($('plugin__searchindex_btn'),'click',plugin_searchindex.go);
     },
 
     /**
      * Gives textual feedback
      */
     status: function(text){
-        pl_si.output.innerHTML = text;
+        plugin_searchindex.output.innerHTML = text;
     },
 
     /**
@@ -48,11 +48,11 @@ var pl_si = {
         var ok = this.response;
         if(ok == 1){
             // start indexing
-            window.setTimeout(pl_si.index,1000);
+            window.setTimeout(plugin_searchindex.index,1000);
         }else{
-            pl_si.status(ok);
+            plugin_searchindex.status(ok);
             // retry
-            window.setTimeout(pl_si.clear,5000);
+            window.setTimeout(plugin_searchindex.clear,5000);
         }
     },
 
@@ -63,15 +63,15 @@ var pl_si = {
      */
     cb_pages: function(){
         var data = this.response;
-        pl_si.pages = data.split("\n");
-        pl_si.count = pl_si.pages.length;
-        pl_si.status(pl_si.lang.pages.replace(/%d/,pl_si.pages.length));
+        plugin_searchindex.pages = data.split("\n");
+        plugin_searchindex.count = plugin_searchindex.pages.length;
+        plugin_searchindex.status(plugin_searchindex.lang.pages.replace(/%d/,plugin_searchindex.pages.length));
 
         // move the first page from the queue
-        pl_si.page = pl_si.pages.shift();
+        plugin_searchindex.page = plugin_searchindex.pages.shift();
 
         // start index cleaning
-        window.setTimeout(pl_si.clear,1000);
+        window.setTimeout(plugin_searchindex.clear,1000);
     },
 
     /**
@@ -84,30 +84,30 @@ var pl_si = {
         var wait = 500;
         if(ok == 1){
             // next page from queue
-            pl_si.page = pl_si.pages.shift();
-            pl_si.done++;
+            plugin_searchindex.page = plugin_searchindex.pages.shift();
+            plugin_searchindex.done++;
         }else{
             // something went wrong, show message
-            pl_si.status(ok);
+            plugin_searchindex.status(ok);
             wait = 5000;
         }
         // next index run
-        window.setTimeout(pl_si.index,500);
+        window.setTimeout(plugin_searchindex.index,500);
     },
 
     /**
      * Starts the indexing of a page.
      */
     index: function(){
-        if(pl_si.page){
-            pl_si.status(pl_si.lang.indexing+' <b>'+pl_si.page+'</b> ('+pl_si.done+'/'+pl_si.count+')');
-            pl_si.sack.onCompletion = pl_si.cb_index;
-            pl_si.sack.URLString = '';
-            pl_si.sack.runAJAX('call=indexpage&page='+encodeURI(pl_si.page));
+        if(plugin_searchindex.page){
+            plugin_searchindex.status(plugin_searchindex.lang.indexing+' <b>'+plugin_searchindex.page+'</b> ('+plugin_searchindex.done+'/'+plugin_searchindex.count+')');
+            plugin_searchindex.sack.onCompletion = plugin_searchindex.cb_index;
+            plugin_searchindex.sack.URLString = '';
+            plugin_searchindex.sack.runAJAX('call=indexpage&page='+encodeURI(plugin_searchindex.page));
         }else{
             // we're done
-            pl_si.throbber_off();
-            pl_si.status(pl_si.lang.done);
+            plugin_searchindex.throbber_off();
+            plugin_searchindex.status(plugin_searchindex.lang.done);
         }
     },
 
@@ -115,41 +115,41 @@ var pl_si = {
      * Cleans the index
      */
     clear: function(){
-        pl_si.status(pl_si.lang.clearing);
-        pl_si.sack.onCompletion = pl_si.cb_clear;
-        pl_si.sack.URLString = '';
-        pl_si.sack.runAJAX('call=clearindex');
+        plugin_searchindex.status(plugin_searchindex.lang.clearing);
+        plugin_searchindex.sack.onCompletion = plugin_searchindex.cb_clear;
+        plugin_searchindex.sack.URLString = '';
+        plugin_searchindex.sack.runAJAX('call=clearindex');
     },
 
     /**
      * Starts the whole index rebuild process
      */
     go: function(){
-        pl_si.throbber_on();
-        pl_si.status(pl_si.lang.finding);
-        pl_si.sack.onCompletion = pl_si.cb_pages;
-        pl_si.sack.URLString = '';
-        pl_si.sack.runAJAX('call=pagelist');
+        plugin_searchindex.throbber_on();
+        plugin_searchindex.status(plugin_searchindex.lang.finding);
+        plugin_searchindex.sack.onCompletion = plugin_searchindex.cb_pages;
+        plugin_searchindex.sack.URLString = '';
+        plugin_searchindex.sack.runAJAX('call=pagelist');
     },
 
     /**
      * add a throbber image
      */
     throbber_on: function(){
-        pl_si.output.style['background-image'] = "url('"+DOKU_BASE+'lib/images/throbber.gif'+"')";
-        pl_si.output.style['background-repeat'] = 'no-repeat';
+        plugin_searchindex.output.style['background-image'] = "url('"+DOKU_BASE+'lib/images/throbber.gif'+"')";
+        plugin_searchindex.output.style['background-repeat'] = 'no-repeat';
     },
 
     /**
      * Stop the throbber
      */
     throbber_off: function(){
-        pl_si.output.style['background-image'] = 'none';
+        plugin_searchindex.output.style['background-image'] = 'none';
     }
 };
 
 addInitEvent(function(){
-    pl_si.init();
+    plugin_searchindex.init();
 });
 
 
