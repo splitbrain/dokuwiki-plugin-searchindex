dbd73a8f97a8b3d48e75fd3622b48e2d2e1d8582
Nicolas Friedli <nicolas@theologique.ch>
2013-12-12 16:05:06 +0100

    translation update

--- lang/fr/lang.php
+++ lang/fr/lang.php
@@ -1,17 +1,19 @@
 <?php
-/**
- * French language file
- */
- 
-// settings must be present and set appropriately for the language
-$lang['encoding']   = 'utf-8';
-$lang['direction']  = 'ltr';
- 
-// for admin plugins, the menu prompt to be displayed in the admin menu
-// if set here, the plugin doesn't need to override the getMenuText() method
-$lang['menu'] = 'Gestionnaire d\'index de recherche'; 
- 
-// custom language strings for the plugin
-$lang['rebuild_index'] = 'Reconstruire l\'index';
 
-//Setup VIM: ex: et ts=4 enc=utf-8 :
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Nicolas Friedli <nicolas@theologique.ch>
+ */
+$lang['menu']                  = 'Gestionnaire d\'index de recherche';
+$lang['update']                = 'Mise à jour de l\'index';
+$lang['update_tip']            = 'Mise à jour de l\'index de recherche seulement (en général, plus rapide)';
+$lang['rebuild']               = 'Reconstruire l\'index';
+$lang['rebuild_tip']           = 'Effacer puis reconstruire complètement l\'index de recherche';
+$lang['js']['indexing']        = 'Indexé :';
+$lang['js']['done']            = 'Indexation terminée.';
+$lang['js']['pages']           = '%d pages trouvées.';
+$lang['js']['clearing']        = 'Nettoyage de l\'index...';
+$lang['js']['finding']         = 'Recherche de pages en cours...';
+$lang['js']['notindexed']      = 'Étape sautée : à jour.';
+$lang['js']['indexed']         = 'Mis à jour';
