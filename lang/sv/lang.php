55c8a518fd0da36686c45b2ca9ae6afc66b7e00f
Tor Härnqvist <tor@harnqvist.se>
2017-07-16 14:30:29 +0200

    translation update

---
+++ lang/sv/lang.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Tor Härnqvist <tor@harnqvist.se>
+ */
+$lang['menu']                  = 'Sökindexhanterare';
+$lang['update']                = 'Uppdatera index';
+$lang['update_tip']            = 'Uppdatera enbart sökindex (oftast snabbare)';
+$lang['rebuild']               = 'Återuppbygger index';
+$lang['rebuild_tip']           = 'Rensa och återuppbygg därefter fullständigt sökindex';
+$lang['js']['indexing']        = 'Indexerade:';
+$lang['js']['done']            = 'Indexering avslutad.';
+$lang['js']['pages']           = '%d sidor hittades.';
+$lang['js']['clearing']        = 'Rensar index...';
+$lang['js']['finding']         = 'Hittar sidor...';
+$lang['js']['notindexed']      = 'Hoppades över: Redan uppdaterad';
+$lang['js']['indexed']         = 'Uppdaterad';
