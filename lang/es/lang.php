8b458a98f5acb8f5c39e935bd14520e4a02a1adc
Domingo Redal <docxml@gmail.com>
2017-09-14 19:05:05 +0200

    translation update

---
+++ lang/es/lang.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Domingo Redal <docxml@gmail.com>
+ */
+$lang['menu']                  = 'Administrador del Índice de Búsqueda';
+$lang['update']                = 'Actualizar el Índice';
+$lang['update_tip']            = 'Solo actualiza el índice de búsqueda (por lo general más rápido)';
+$lang['rebuild']               = 'Regenerar el Índice';
+$lang['rebuild_tip']           = 'Borrar completamente y luego reconstruir el índice de búsqueda';
+$lang['js']['indexing']        = 'Indexadas:';
+$lang['js']['done']            = 'Terminada la indexación.';
+$lang['js']['pages']           = '%d páginas encontradas.';
+$lang['js']['clearing']        = 'Borrado de índice...';
+$lang['js']['finding']         = 'Buscando páginas...';
+$lang['js']['notindexed']      = 'Saltada: Al día';
+$lang['js']['indexed']         = 'Actualizado';
