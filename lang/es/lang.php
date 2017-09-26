0de3547bf1f153f4809334b9253662f4fb5e09b6
Domingo Redal <docxml@gmail.com>
2017-09-26 16:00:17 +0200

    translation update

--- lang/es/lang.php
+++ lang/es/lang.php
@@ -5,15 +5,15 @@
  *
  * @author Domingo Redal <docxml@gmail.com>
  */
-$lang['menu']                  = 'Administrador del Índice de Búsqueda';
-$lang['update']                = 'Actualizar el Índice';
+$lang['menu']                  = 'Administrador del índice de búsqueda';
+$lang['update']                = 'Actualizar el índice';
 $lang['update_tip']            = 'Solo actualiza el índice de búsqueda (por lo general más rápido)';
-$lang['rebuild']               = 'Regenerar el Índice';
+$lang['rebuild']               = 'Regenerar el índice';
 $lang['rebuild_tip']           = 'Borrar completamente y luego reconstruir el índice de búsqueda';
 $lang['js']['indexing']        = 'Indexadas:';
 $lang['js']['done']            = 'Terminada la indexación.';
 $lang['js']['pages']           = '%d páginas encontradas.';
 $lang['js']['clearing']        = 'Borrado de índice...';
 $lang['js']['finding']         = 'Buscando páginas...';
-$lang['js']['notindexed']      = 'Saltada: Al día';
+$lang['js']['notindexed']      = 'Saltada: al día';
 $lang['js']['indexed']         = 'Actualizado';
