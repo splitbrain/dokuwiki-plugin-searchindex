8ef7b564343c443263cbd510433754a74f9e8fdb
Robert Surý <rsurycz@seznam.cz>
2019-08-01 18:00:49 +0200

    translation update

---
+++ lang/cs/lang.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Robert Surý <rsurycz@seznam.cz>
+ */
+$lang['update']                = 'Aktualizovat index';
+$lang['update_tip']            = 'Jen aktualizovat vyhledávací index (obvykle rychlejší)';
+$lang['rebuild']               = 'Znovu vytvořit index';
+$lang['rebuild_tip']           = 'Vymazat a poté znovu vytvořit vyhledávací index';
+$lang['js']['indexing']        = 'Indexováno:';
+$lang['js']['done']            = 'Dokončeno indexování.';
+$lang['js']['pages']           = '%d stránek nalezeno.';
+$lang['js']['clearing']        = 'Vymazávání indexu...';
+$lang['js']['finding']         = 'Hledání stránek...';
+$lang['js']['notindexed']      = 'Přeskočeno: aktuální';
+$lang['js']['indexed']         = 'Aktualizováno';
