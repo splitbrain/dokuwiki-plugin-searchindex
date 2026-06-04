860271ebd5dc2e10cf6912bd08a8cff7151b2563
Wizzard <wizzardsk@gmail.com>
2026-06-04 20:30:16 +0200

    Translation update (sk)

---
+++ lang/sk/lang.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * @license    GPL 2 (https://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Wizzard <wizzardsk@gmail.com>
+ */
+$lang['menu']                  = 'Správca Searchindex';
+$lang['update']                = 'Aktualizovať index';
+$lang['update_tip']            = 'Iba aktualizovať vyhľadávací index (zvyčajne rýchlejšie)';
+$lang['rebuild']               = 'Prebudovať index';
+$lang['rebuild_tip']           = 'Vyčistiť a potom úplne prebudovať vyhľadávací index';
+$lang['indexing']              = 'Zindexované:';
+$lang['done']                  = 'Indexovanie dokončené.';
+$lang['pages']                 = 'Nájdených %d stránok.';
+$lang['clearing']              = 'Čistí sa index...';
+$lang['finding']               = 'Hľadajú sa stránky...';
+$lang['notindexed']            = 'Preskočené: aktuálne';
+$lang['indexed']               = 'Aktualizované';
